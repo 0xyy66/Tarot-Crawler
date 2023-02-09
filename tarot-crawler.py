fa93039c23ea0cdd7738d331a3f6623e33af111a
0xyy66 <publichousing0000@gmail.com>
2023-02-09 23:02:19 +0100

    first commit

---
+++ tarot-crawler.py
@@ -0,0 +1,182 @@
+import httpx
+from bs4 import BeautifulSoup
+import json
+
+
+'''
+{
+    MajorArcana:
+        [
+            {
+                card:
+                short_info:
+                    upright:
+                    reversed:
+                    description:
+                info:
+                    upright:
+                    reversed:
+            }
+        ]
+    MinorArcana:
+        {
+            Wands:
+                [
+                    {
+                        card:
+                        short_info:
+                            {
+                                upright:
+                                reversed:
+                                description:
+                            }
+                        info:
+                            {
+                                upright:
+                                reversed:
+                            }
+                    }
+
+                ]
+            ...
+        }
+}
+'''
+
+def get_major_arcana_urls(client):
+    base_url = 'https://www.biddytarot.com'
+    res = client.get(base_url + '/tarot-card-meanings/major-arcana/')
+    print('[x] Major Arcana scraping')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    a_tags = soup.find_all('a')
+    urls = []
+    for a in a_tags:
+        if 'major-arcana' in a['href']:
+            urls.append(base_url + a['href'])
+    return urls
+
+def get_minor_arcana_urls(client, seed):
+    base_url = 'https://www.biddytarot.com'
+    res = client.get(base_url + f'/tarot-card-meanings/minor-arcana/suit-of-{seed}/')
+    print('[x] Minor Arcana scraping')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    a_tags = soup.find_all('a')
+    urls = []
+    for a in a_tags:
+        if 'minor-arcana' in a['href']:
+            urls.append(base_url + a['href'])
+    del urls[0]
+    return urls
+
+def get_major_arcana_info(client, urls):
+    MajorArcana = []
+    for url in urls:
+        res = client.get(url)
+        print(f'[x] MA - {url.split("/")[-2]} scraping')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        mobhide = soup.find_all('div', {'class': 'mobilehide'})
+        card_name = mobhide[0].find('h3').contents[0].replace('Keywords', '')
+        short_info = mobhide[0].find_all('p')
+        card = {
+            'card': card_name,
+            'short_info':
+                {
+                    'upright': '',
+                    'reversed': '',
+                    'description': ''
+                },
+            'info':
+                {
+                    'upright': '',
+                    'reversed': ''
+                }
+        }
+        for p in short_info:
+            if 'UPRIGHT:' in str(p):
+                card['short_info']['upright'] = ((str(p)).split('</span>')[1].split('</p>')[0]).strip()
+            elif 'REVERSED:' in str(p):
+                card['short_info']['reversed'] = ((str(p)).split('</span>')[1].split('</p>')[0]).strip()
+            elif 'NOTE:' not in p:
+                card['short_info']['description'] += p.contents[0].strip() + ' '
+        post_area = soup.find_all('div', {'class': 'row'})
+        for p in post_area[4].find_all('p'):
+            card['info']['upright'] += p.contents[0].strip() + ' '
+        for p in post_area[5].find_all('p'):
+            card['info']['reversed'] += p.contents[0].strip() + ' '
+        # remove space at the end
+        card['short_info']['description'] = card['short_info']['description'].strip()
+        card['info']['upright'] = card['info']['upright'].strip()
+        card['info']['reversed'] = card['info']['reversed'].strip()
+        MajorArcana.append(card)
+        return MajorArcana
+
+def get_minor_arcana_info(client, urls, seed):
+    suit_of_seed = []
+    for url in urls:
+        res = client.get(url)
+        print(f'[x] ma - {seed} - {url.split("/")[-2]} scraping')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        mobhide = soup.find_all('div', {'class': 'mobilehide'})
+        card_name = mobhide[0].find('h3').contents[0].replace('Keywords', '')
+        short_info = mobhide[0].find_all('p')
+        card = {
+            'card': card_name,
+            'short_info':
+                {
+                    'upright': '',
+                    'reversed': '',
+                    'description': ''
+                },
+            'info':
+                {
+                    'upright': '',
+                    'reversed': ''
+                }
+        }
+        for p in short_info:
+            if 'UPRIGHT:' in str(p):
+                card['short_info']['upright'] = ((str(p)).split('</span>')[1].split('</p>')[0]).strip()
+            elif 'REVERSED:' in str(p):
+                card['short_info']['reversed'] = ((str(p)).split('</span>')[1].split('</p>')[0]).strip()
+            elif 'NOTE:' not in p:
+                card['short_info']['description'] += p.contents[0].strip() + ' '
+        post_area = soup.find_all('div', {'class': 'row'})
+        for p in post_area[4].find_all('p'):
+            card['info']['upright'] += p.contents[0].strip() + ' '
+        for p in post_area[5].find_all('p'):
+            card['info']['reversed'] += p.contents[0].strip() + ' '
+        # remove space at the end
+        card['short_info']['description'] = card['short_info']['description'].strip()
+        card['info']['upright'] = card['info']['upright'].strip()
+        card['info']['reversed'] = card['info']['reversed'].strip()
+        #print(card)
+        suit_of_seed.append(card)
+    return suit_of_seed
+
+if __name__ == '__main__':
+    client = httpx.Client()
+    client.headers = {
+        "User-Agent": "Tarot Crawler. Source code on GitHub soon!"
+        #"User-Agent": "Mozilla/5.0 (Linux; Android 11; Pixel 5 Build/RQ3A.210805.001.A1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.159 Mobile Safari/537.36"
+    }
+    MA_urls = get_major_arcana_urls(client)
+    #MA_urls = ['https://www.biddytarot.com/tarot-card-meanings/major-arcana/fool/']
+    MajorArcana = get_major_arcana_info(client, MA_urls)
+    minorArcana = {}
+    ma_urls = get_minor_arcana_urls(client, seed='cups')
+    minorArcana['cups'] = get_minor_arcana_info(client, ma_urls, seed='cups')
+    #print(minorArcana)
+    ma_urls = get_minor_arcana_urls(client, seed='swords')
+    minorArcana['swords'] = get_minor_arcana_info(client, ma_urls, seed='swords')
+    ma_urls = get_minor_arcana_urls(client, seed='pentacles')
+    minorArcana['pentacles'] = get_minor_arcana_info(client, ma_urls, seed='pentacles')
+    ma_urls = get_minor_arcana_urls(client, seed='wands')
+    minorArcana['wands'] = get_minor_arcana_info(client, ma_urls, seed='wands')
+    deck = {
+        "major_arcana": MajorArcana,
+        "minor_arcana": minorArcana 
+    }
+    with open('tarot_deck.json', 'w') as f:
+        json.dump(deck, f)
+
+
